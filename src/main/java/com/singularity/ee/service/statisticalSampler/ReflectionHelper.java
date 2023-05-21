6d7705a3ef8792f7ac350a7c8b1997babfda13bc
John Southerland <jsoutherland@me.com>
2023-05-21 11:31:05 -0500

    reflective changes working on java 8, still need to compile a java 9+ version with module support

--- src/main/java/com/singularity/ee/service/statisticalSampler/ReflectionHelper.java
+++ src/main/java/com/singularity/ee/service/statisticalSampler/ReflectionHelper.java
@@ -13,23 +13,56 @@ public class ReflectionHelper {
     public static void setMaxEvents( Object eventServiceObject, int newMaxEvents ) {
         try {
             Field field = eventServiceObject.getClass().getDeclaredField("maxEventSize");
-            field.setAccessible(true);
-            Field modifiersField = Field.class.getDeclaredField("modifiers"); //these are private after java 1.8
+
+            // Remove the final modifier
+            Field modifiersField = Field.class.getDeclaredField("modifiers");
             modifiersField.setAccessible(true);
             modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
-            field.set(eventServiceObject, newMaxEvents);
+
+            // Make the field accessible
+            field.setAccessible(true);
+
+            // Update the field value
+            field.setInt(eventServiceObject, newMaxEvents);
+
         } catch (Exception e) {
             logger.error(String.format("Can not set the maxEventSize to %d, Exception: %s",newMaxEvents,e.getMessage()),e);
         }
+    }
+
+    public static void setMaxEventsJava9(Object object, int newValue) throws NoSuchFieldException, IllegalAccessException {
+        Field field = object.getClass().getDeclaredField("maxEventsSize");
+
+        // Remove the final modifier
+        Field modifiersField = Field.class.getDeclaredField("modifiers");
+        modifiersField.setAccessible(true);
+        modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+
+        // Make the field accessible
+        field.setAccessible(true);
+
+        // Update the field value
+        field.setInt(object, newValue);
+
+        // Update the module to allow access to the field
+        /* TODO for java9+
+        Module module = field.getDeclaringClass().getModule();
+        Module unnamedModule = ReflectionExample.class.getModule();
+        unnamedModule.addReads(module);
+
+         */
     }
 
     public static int getMaxEvents( Object eventServiceObject ) {
         try {
             Field field = eventServiceObject.getClass().getDeclaredField("maxEventSize");
             field.setAccessible(true);
+            /*
             Field modifiersField = Field.class.getDeclaredField("modifiers"); //these are private after java 1.8
             modifiersField.setAccessible(true);
             modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+
+             */
             return (int) field.get(eventServiceObject);
         } catch (Exception e) {
             logger.error(String.format("Can not get the maxEventSize from object sending from system property, Exception: %s",e.getMessage()),e);
