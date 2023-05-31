70adef5cd0394871f02b28f761c37196ade39913
John Southerland <jsoutherland@me.com>
2023-05-31 15:58:22 -0600

    fixed some wild reflection and added a method to report metrics to a metric aggregator

--- src/main/java/com/singularity/ee/service/statisticalSampler/ReflectionHelper.java
+++ src/main/java/com/singularity/ee/service/statisticalSampler/ReflectionHelper.java
@@ -16,7 +16,7 @@ public class ReflectionHelper {
         try {
             modifyPrivateFinalInt(eventServiceObject, "maxEventSize", newMaxEvents);
         } catch (Exception e) {
-            logger.error(String.format("Can not set the maxEventSize to %d, Exception: %s",newMaxEvents,e.getMessage()),e);
+            logger.warn(String.format("Can not set the maxEventSize to %d, Exception: %s",newMaxEvents,e.getMessage()),e);
         }
     }
 
@@ -24,7 +24,7 @@ public class ReflectionHelper {
         try {
             return getPrivateFinalInt(eventServiceObject, "maxEventSize");
         } catch (Exception e) {
-            logger.error(String.format("Can not get the maxEventSize from object sending from system property, Exception: %s",e.getMessage()),e);
+            logger.warn(String.format("Can not get the maxEventSize from object sending from system property, Exception: %s",e.getMessage()),e);
             return SystemUtilsTranslateable.getIntProperty("appdynamics.agent.maxEvents", 100);
         }
     }
@@ -35,11 +35,11 @@ public class ReflectionHelper {
 
         // Check the Java version at runtime
         if (isJava9OrAbove()) {
+            // Update the module to allow access to the field
+            updateModuleAccessJava9Plus(object, "com.singularity.ee.agent.commonservices.eventgeneration");
+
             // Remove the final modifier using Java 9+ specific logic
             removeFinalModifierJava9Plus(field);
-
-            // Update the module to allow access to the field
-            updateModuleAccessJava9Plus(field);
         } else {
             // Remove the final modifier using Java 8 specific logic
             removeFinalModifierJava8(field);
@@ -62,8 +62,17 @@ public class ReflectionHelper {
         field.setInt(field, field.getModifiers() & ~Modifier.FINAL);
     }
 
-    private static void updateModuleAccessJava9Plus(Field field) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        Class<?> declaringClass = field.getDeclaringClass();
+    private static void updateModuleAccessJava9Plus(Object myObject, String addOpensWhat ) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+
+        Class<?> moduleClass = Class.forName("java.lang.Module");
+        Method getModuleMethod = Class.class.getMethod("getModule");
+        Object declaringModule = getModuleMethod.invoke(myObject.getClass());
+        Object currentModule = getModuleMethod.invoke(ReflectionHelper.class);
+
+        Method addOpensMethod = currentModule.getClass().getMethod("addOpens", String.class, moduleClass);
+        addOpensMethod.invoke(currentModule, addOpensWhat, declaringModule);
+
+        /*
         Class<?> reflectionHelperClass = ReflectionHelper.class;
         Object declaringModule = declaringClass.getClassLoader()
                 .loadClass("java.lang.Module")
@@ -75,7 +84,9 @@ public class ReflectionHelper {
                 .invoke(reflectionHelperClass);
 
         Method addOpensMethod = reflectionExampleModule.getClass().getMethod("addOpens", String.class, reflectionExampleModule.getClass());
-        addOpensMethod.invoke(reflectionExampleModule, declaringClass.getPackage().getName(), declaringModule);
+        addOpensMethod.invoke(reflectionExampleModule, addOpensWhat, declaringModule);
+
+         */
     }
 
     private static boolean isJava9OrAbove() {
@@ -89,12 +100,28 @@ public class ReflectionHelper {
         return majorVersion > 8;
     }
 
-    public static int getPrivateFinalInt(Object object, String fieldName) throws NoSuchFieldException, IllegalAccessException {
+    public static int getPrivateFinalInt(Object object, String fieldName) throws NoSuchFieldException, IllegalAccessException, ClassNotFoundException, InvocationTargetException, NoSuchMethodException {
         Field field = object.getClass().getDeclaredField(fieldName);
+        if ( isJava9OrAbove() )
+            updateModuleAccessJava9Plus(object, "com.singularity.ee.agent.commonservices.eventgeneration");
         field.setAccessible(true);
-
         // Retrieve the field value
         return field.getInt(object);
     }
 
+    public static void addReportMetric( Object metricAggregator, long value ) {
+
+        Method reportMethod = null;
+        try {
+            reportMethod = metricAggregator.getClass().getDeclaredMethod("_report", long.class);
+            if (isJava9OrAbove()) {
+                updateModuleAccessJava9Plus( metricAggregator, "com.singularity.ee.agent.commonservices.metricgeneration.aggregation");
+            }
+            reportMethod.setAccessible(true);
+            reportMethod.invoke(metricAggregator, value);
+        } catch (Exception e) {
+            logger.warn(String.format("Can not call %s._report(%d), Exception: %s",metricAggregator.getClass().getName(), value, e.getMessage()), e);
+        }
+
+    }
 }
