f9521767dd443153e57181cb8457e5bee6e61a4e
John Southerland <john.southerland@appdynamics.com>
2023-06-08 13:33:47 -0600

    parameterized the enable node property to keep from having silly errors in the future

--- src/main/java/com/singularity/ee/service/statisticalSampler/AgentNodeProperties.java
+++ src/main/java/com/singularity/ee/service/statisticalSampler/AgentNodeProperties.java
@@ -11,14 +11,15 @@ import java.util.Observable;
 
 public class AgentNodeProperties extends Observable {
     private static final IADLogger logger = ADLoggerFactory.getLogger((String)"com.singularity.dynamicservice.statisticalSampler.AgentNodeProperties");
+    public static final String ENABLE_PROPERTY = "agent.statisticalSampler.enabled";
     public static final String[] NODE_PROPERTIES = new String[]{"agent.statisticalSampler.enabled", "agent.statisticalSampler.percentage", "agent.statisticalSampler.maxEvents", "agent.statisticalSampler.decisionDurationMinutes", "agent.statisticalSampler.holdMaxEvents", "agent.statisticalSampler.isMetricThrottled", "agent.statisticalSampler.isEventThrottled"};
     private final Map<String, String> properties = new HashMap<>();
 
     public void initializeConfigs(IServiceConfig serviceConfig) {
         Map configProperties = serviceConfig.getConfigProperties();
         if( configProperties != null ) {
-            boolean enabled = StringOperations.safeParseBoolean((String)((String)configProperties.get("agent.statisticalSampler.enabled")), (boolean)false);
-            this.properties.put("agent.statisticalSampler.enabled", Boolean.toString(enabled));
+            boolean enabled = StringOperations.safeParseBoolean((String)((String)configProperties.get(ENABLE_PROPERTY)), (boolean)false);
+            this.properties.put(ENABLE_PROPERTY, Boolean.toString(enabled));
             this.properties.put("agent.statisticalSampler.percentage", (String)configProperties.get("agent.statisticalSampler.percentage"));
             this.properties.put("agent.statisticalSampler.maxEvents", (String)configProperties.get("agent.statisticalSampler.maxEvents"));
             this.properties.put("agent.statisticalSampler.decisionDurationMinutes", (String)configProperties.get("agent.statisticalSampler.decisionDurationMinutes"));
@@ -53,7 +54,7 @@ public class AgentNodeProperties extends Observable {
     }
 
     public boolean isEnabled() {
-        return StringOperations.safeParseBoolean((String)this.getProperty("agent.statisticalSampler.enabled"), (boolean)false);
+        return StringOperations.safeParseBoolean((String)this.getProperty(ENABLE_PROPERTY), (boolean)false);
     }
 
     public boolean isMaxEventsSet() { return this.properties.get("agent.statisticalSampler.maxEvents") != null; }
